1b733cef29bad9a4c4ae73aa3683b9c5a3b973dc
CoreRasurae <38363426+CoreRasurae@users.noreply.github.com>
2021-10-12 17:56:28 +0100

    Fix: Execution of Farneback on Windows 64 bits

--- src/GaussianKernelBitExact.py
+++ src/GaussianKernelBitExact.py
@@ -49,8 +49,8 @@ import numpy as np
 import struct
 from decimal import *
 
-softOne = Decimal(struct.unpack('d', struct.pack('l', 1023 << 52))[0])
-softZero = Decimal(struct.unpack('d', struct.pack('l', 0))[0])
+softOne = Decimal('1.0')  #struct.unpack('d', struct.pack('q', 1023 << 52))[0]
+softZero = Decimal('0.0') #struct.unpack('d', struct.pack('q', 0))[0]
 
 def getGaussianKernelBitExact(n, sigma):
     assert(n > 0)
@@ -59,45 +59,45 @@ def getGaussianKernelBitExact(n, sigma):
         if n == 1:
             return softOne, np.array([softOne])
         elif n == 3:
-            result = np.array( [ struct.unpack('d', struct.pack('l', 0x3fd0000000000000))[0], #0.25
-                                 struct.unpack('d', struct.pack('l', 0x3fe0000000000000))[0], #0.50
-                                 struct.unpack('d', struct.pack('l', 0x3fd0000000000000))[0], #0.25
+            result = np.array( [ struct.unpack('d', struct.pack('q', 0x3fd0000000000000))[0], #0.25
+                                 struct.unpack('d', struct.pack('q', 0x3fe0000000000000))[0], #0.50
+                                 struct.unpack('d', struct.pack('q', 0x3fd0000000000000))[0], #0.25
                                ], dtype=np.float64 )
             return softOne, result
         elif n == 5:
-            result = np.array( [ struct.unpack('d', struct.pack('l', 0x3fb0000000000000))[0], #0.0625
-                                 struct.unpack('d', struct.pack('l', 0x3fd0000000000000))[0], #0.25
-                                 struct.unpack('d', struct.pack('l', 0x3fd8000000000000))[0], #0.375
-                                 struct.unpack('d', struct.pack('l', 0x3fd0000000000000))[0], #0.25
-                                 struct.unpack('d', struct.pack('l', 0x3fb0000000000000))[0], #0.0625
+            result = np.array( [ struct.unpack('d', struct.pack('q', 0x3fb0000000000000))[0], #0.0625
+                                 struct.unpack('d', struct.pack('q', 0x3fd0000000000000))[0], #0.25
+                                 struct.unpack('d', struct.pack('q', 0x3fd8000000000000))[0], #0.375
+                                 struct.unpack('d', struct.pack('q', 0x3fd0000000000000))[0], #0.25
+                                 struct.unpack('d', struct.pack('q', 0x3fb0000000000000))[0], #0.0625
                                ], dtype=np.float64 )
             return softOne, result
         elif n == 7:
-            result = np.array( [ struct.unpack('d', struct.pack('l', 0x3fa0000000000000))[0], #0.03125
-                                 struct.unpack('d', struct.pack('l', 0x3fbc000000000000))[0], #0.109375
-                                 struct.unpack('d', struct.pack('l', 0x3fcc000000000000))[0], #0.21875
-                                 struct.unpack('d', struct.pack('l', 0x3fd2000000000000))[0], #0.28125
-                                 struct.unpack('d', struct.pack('l', 0x3fcc000000000000))[0], #0.21875
-                                 struct.unpack('d', struct.pack('l', 0x3fbc000000000000))[0], #0.109375
-                                 struct.unpack('d', struct.pack('l', 0x3fa0000000000000))[0], #0.03125
+            result = np.array( [ struct.unpack('d', struct.pack('q', 0x3fa0000000000000))[0], #0.03125
+                                 struct.unpack('d', struct.pack('q', 0x3fbc000000000000))[0], #0.109375
+                                 struct.unpack('d', struct.pack('q', 0x3fcc000000000000))[0], #0.21875
+                                 struct.unpack('d', struct.pack('q', 0x3fd2000000000000))[0], #0.28125
+                                 struct.unpack('d', struct.pack('q', 0x3fcc000000000000))[0], #0.21875
+                                 struct.unpack('d', struct.pack('q', 0x3fbc000000000000))[0], #0.109375
+                                 struct.unpack('d', struct.pack('q', 0x3fa0000000000000))[0], #0.03125
                                ], dtype=np.float64 )
             return softOne, result
         elif n == 9:
-            result = np.array( [ struct.unpack('d', struct.pack('l', 0x3f90000000000000))[0], #4  / 256
-                                 struct.unpack('d', struct.pack('l', 0x3faa000000000000))[0], #13 / 256
-                                 struct.unpack('d', struct.pack('l', 0x3fbe000000000000))[0], #30 / 256
-                                 struct.unpack('d', struct.pack('l', 0x3fc9800000000000))[0], #51 / 256
-                                 struct.unpack('d', struct.pack('l', 0x3fce000000000000))[0], #60 / 256
-                                 struct.unpack('d', struct.pack('l', 0x3fc9800000000000))[0], #51 / 256
-                                 struct.unpack('d', struct.pack('l', 0x3fbe000000000000))[0], #30 / 256
-                                 struct.unpack('d', struct.pack('l', 0x3faa000000000000))[0], #13 / 256
-                                 struct.unpack('d', struct.pack('l', 0x3f90000000000000))[0], #4  / 256
+            result = np.array( [ struct.unpack('d', struct.pack('q', 0x3f90000000000000))[0], #4  / 256
+                                 struct.unpack('d', struct.pack('q', 0x3faa000000000000))[0], #13 / 256
+                                 struct.unpack('d', struct.pack('q', 0x3fbe000000000000))[0], #30 / 256
+                                 struct.unpack('d', struct.pack('q', 0x3fc9800000000000))[0], #51 / 256
+                                 struct.unpack('d', struct.pack('q', 0x3fce000000000000))[0], #60 / 256
+                                 struct.unpack('d', struct.pack('q', 0x3fc9800000000000))[0], #51 / 256
+                                 struct.unpack('d', struct.pack('q', 0x3fbe000000000000))[0], #30 / 256
+                                 struct.unpack('d', struct.pack('q', 0x3faa000000000000))[0], #13 / 256
+                                 struct.unpack('d', struct.pack('q', 0x3f90000000000000))[0], #4  / 256
                               ], dtype=np.float64 )
             return softOne, result            
 
-    d0_15 = Decimal(struct.unpack('d', struct.pack('l', 0x3fc3333333333333))[0]) #0.15
-    d0_35 = Decimal(struct.unpack('d', struct.pack('l', 0x3fd6666666666666))[0]) #0.35
-    dminus_0_125 = Decimal(struct.unpack('d', struct.pack('L', 0xbfc0000000000000))[0]) #-0.5*0.25
+    d0_15 = Decimal('0.15')          #struct.unpack('d', struct.pack('q', 0x3fc3333333333333))[0] #0.15
+    d0_35 = Decimal('0.35')          #struct.unpack('d', struct.pack('q', 0x3fd6666666666666))[0] #0.35
+    dminus_0_125 = Decimal('-0.125') #struct.unpack('d', struct.pack('Q', 0xbfc0000000000000))[0] #-0.5*0.25
     #Calculus should be done directly with software bit manipulation only
     sigmaX = Decimal(0.0)
     if sigma < 0:
